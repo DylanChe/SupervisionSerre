fccbf18e7103a3afb7b91c2b9937be4398d37b3e
vesty44 <33455651+vesty44@users.noreply.github.com>
2018-04-05 16:47:40 +0200

    MAJ

---
+++ python_co_bdd/supervision_serre.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.4
+# coding: utf-8
+"""
+Programme : AmazingNews.py		version 0.5
+Date : 07-12-2017
+"""
+
+from datetime import datetime
+import mysql.connector
+import RPi.GPIO as GPIO
+import connexion_bdd
+
+
+try:
+	while(1)
+		query = "INSERT INTO materiel (id, nom, etat, id_type_materiel) VALUES ('9', '"  Potentiomètre  "', '0', '1')"
+		bdd.insertInto(query)
+
+except:
+	print('[ERREUR] Arret du programme...')
+	sys.exit(0)		
