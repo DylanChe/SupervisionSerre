453954087e94b7a8305c6cbf90af439e20ff18dd
RINEAU Willy <33454941+WillyRINEAU@users.noreply.github.com>
2018-05-22 14:30:41 +0200

    MAJ

--- doc_capteurs/boucle_4_20_mA/examples/_4_20mA_03_several_sensors/_4_20mA_03_several_sensors.ino
+++ doc_capteurs/boucle_4_20_mA/examples/_4_20mA_03_several_sensors/_4_20mA_03_several_sensors.ino
@@ -1,6 +1,3 @@
-#line 2 "sketch.ino"
-#include <ArduinoUnit.h>
-
 #include <currentLoop.h>
 
 #define TEMPERATURE_AIR CHANNEL1
@@ -9,15 +6,6 @@
 
 float current;
 float valeur;
-float current_eau_min;
-
-
-test(ok)
-  {
-    current = sensorBoard.readCurrent(CHANNEL1);
-    float min_ma = 4;
-    assertMoreOrEqual(current,min_ma);
-  }
 
 
 void setup()
@@ -40,11 +28,13 @@ void loop()
   {
     // Get the sensor value as a curren in mA
     current = sensorBoard.readCurrent(TEMPERATURE_AIR );
-    Serial.print("La valeur du capteur temperature de l'air est : ");
+    Serial.print("Valeur mesurée : ");
     Serial.print(current);
     Serial.println(" mA");
     valeur = ((current-4)*45)/16;
-    Serial.println(valeur);
+    Serial.print("Le capteur de température de l'air indique: ");
+    Serial.print(valeur);
+    Serial.println(" °C.");
   }
   else {
     Serial.println("Il n'y a pas de capteur connecté au port 1..");
@@ -59,11 +49,13 @@ void loop()
   {
     // Get the sensor value as a curren in mA.
     current = sensorBoard.readCurrent(TEMPERATURE_EAU);
-    Serial.print("La valeur du capteur temperature de l'eau est : ");
+    Serial.print("Valeur mesurée : ");
     Serial.print(current);
     Serial.println(" mA"); 
     valeur = ((current-4)*100)/16;
-    Serial.println(valeur);
+    Serial.print("Le capteur de température de l'eau indique: ");
+    Serial.print(valeur);
+    Serial.println(" °C.");
   }
   else {
     Serial.println("Il n'y a pas de capteur connecté au port 3..");
@@ -74,7 +66,6 @@ void loop()
   Serial.print("\n");
 
   delay(3000);
-  Test::run();
 
 
 }
